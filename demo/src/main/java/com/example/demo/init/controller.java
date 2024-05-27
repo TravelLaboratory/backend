c71d182f7ad8d7b09fb88d91cd05d37ea5b3da7c
yuseonjun <seonjun0906@gmail.com>
2024-05-27 18:50:43 +0900

    feat: test

---
+++ demo/src/main/java/com/example/demo/init/controller.java
@@ -0,0 +1,14 @@
+package com.example.demo.init;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class controller {
+
+    @GetMapping("/")
+    public String hi() {
+        return "gd";
+    }
+}
